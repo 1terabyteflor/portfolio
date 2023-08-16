2d1ad9adce861e40849299e6db8d7b9faf67560a
Flor <florencgmz@gmail.com>
2023-08-16 20:10:51 -0300

    cursor pointer + navigate into project

--- components/Works.tsx
+++ components/Works.tsx
@@ -6,15 +6,14 @@ function Works({image, title, description, date, link}
     : {image: string, title: string, description: string, date: string, link: string}) {
 
   return (
-    <div className='w-full relative flex justify-start align-bottom h-screen overflow-hidden grayscale hover:grayscale-0 hover:contrast-100' id='projects'>
-      <div className='flex flex-col z-30 justify-end align-bottom p-5 w-full'>
+    <div className='w-full relative flex justify-start align-bottom h-screen overflow-hidden grayscale hover:grayscale-0 hover:contrast-100 hover:cursor-' id='projects'>
+      <Link className='flex flex-col z-30 justify-end align-bottom p-5 w-full' href={link} target='_blank'>
         <h1 className='font-pinyon text-6xl text-pink drop-shadow-main'>{title}</h1>
         <div className='font-nunito text-sm uppercase text-pink flex flex-col lg:flex-row justify-between'>
           <p className='lg:w-[50%]'>{description}</p>
           <p className='lg:ml-10'>{date}</p>
-          <Link className='lg:ml-10 links' href={link} target='_blank'>/ go to project </Link>
         </div>
-      </div>
+      </Link>
       <video autoPlay loop muted playsInline className="absolute z-10 min-h-screen max-w-screen object-cover">
         <source src={image} type="video/mp4"/>Your browser does not support the video tag.
       </video>
