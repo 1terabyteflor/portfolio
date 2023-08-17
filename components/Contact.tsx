24e8b39af7ac9e9b6f66b2ba91f4b2fbeacda44f
Flor <florencgmz@gmail.com>
2023-08-16 22:04:13 -0300

    fixes on color

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -9,16 +9,16 @@ function Contact() {
           <div>
             <p className='font-filmotype-yale text-3xl text-pink'>vía:</p>
             <h1 className='font-filmotype-yale text-2xl sm:text-9xl text-pink'>E-Mail</h1>
-            <Link className='font-filmotype-yale links text-xl sm:text-3xl underline' href='mailto:111plantasia111@gmail.com'>111plantasia111@gmail.com</Link>
+            <Link className='font-filmotype-yale links text-xl sm:text-3xl underline text-pink hover:text-white' href='mailto:111plantasia111@gmail.com'>111plantasia111@gmail.com</Link>
             <div className='flex flex-col pb-20'>
-              <Link className='font-filmotype-yale links lg:text-3xl underline pt-20' href='https://instagram.com/plantasia111'>Instagram</Link>
-              <Link className= 'font-filmotype-yale links lg:text-3xl underline pt-2' href='https://vimeo.com/1terabyteflor'>Vimeo</Link>
-              <Link className='font-filmotype-yale links lg:text-3xl underline pt-2' href='https://github.com/1terabyteflor'>Github</Link>
+              <Link className='font-filmotype-yale links lg:text-3xl underline pt-20 text-pink hover:text-white' href='https://instagram.com/plantasia111'>Instagram</Link>
+              <Link className= 'font-filmotype-yale links lg:text-3xl underline pt-2 text-pink hover:text-white' href='https://vimeo.com/1terabyteflor'>Vimeo</Link>
+              <Link className='font-filmotype-yale links lg:text-3xl underline pt-2 text-pink hover:text-white' href='https://github.com/1terabyteflor'>Github</Link>
             </div>
           </div>
           <div className='pr-10 flex flex-col'>
             <p className='font-filmotype-yale text-3xl text-pink'>or download:</p>
-            <Link className='font-filmotype-yale links text-2xl sm:text-7xl underline' href='https://www.notion.so/Florencia-Gomez-82eed371b19347b0a08a0ecafafbb316'>CV</Link>
+            <Link className='font-filmotype-yale links text-2xl sm:text-7xl underline text-pink hover:text-white'  href='https://www.notion.so/Florencia-Gomez-82eed371b19347b0a08a0ecafafbb316'>CV</Link>
           </div>
         </div>
     </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,8 @@ import Link from 'next/link';
 
 function Footer() {
   return (
-    <div className='w-full h-[100px] bg-pink flex items-end justify-between px-2 pb-2 font-bold'>
-        <div className='text-[8px] font-mundial uppercase text-black'>
-            Design by <Link className='hover:text-gray-200' href='https://ivannevares.vercel.app/'>Ivan Nevares</Link>
-
-        </div>
-        <div className='text-[8px] font-mundial uppercase text-black'> 2023 / this website is constantly evolving
+    <div className='w-full h-[50px] bg-pink flex items-end justify-between px-2 pb-2'>
+        <div className='text-[8px] font-mundial uppercase text-black'> 2023 © this website is constantly evolving
         </div>
     </div>
   )
